87ca53fed5f04231d3021aa264e546c8f36073a4
Sara Ebbers <johndoe@example.com>
2018-08-27 15:42:56 -0600

    Set initial files and frameworks

--- test/index-test.js
+++ test/index-test.js
@@ -1,4 +1,4 @@
- import { assert } from 'chai';
+import { assert } from 'chai';
     
  describe('test', function() {
    it('should return true', function() {
---
+++ test/trie-test.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+import Node from '../lib/Node';
+import Trie from '../lib/Trie'
+
+describe('TRIE', () => {
+    let trie;
+
+  beforeEach(() => {
+    trie = new Trie();
+    });
+
+    
+   })
